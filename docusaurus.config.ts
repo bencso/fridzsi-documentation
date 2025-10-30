387b52e9c915359fc49eefcd09190f2af37c0dca
bencso <bencsoofficial@gmail.com>
2025-10-30 15:00:41 +0100

    Add SVG illustration and TypeScript configuration file
    - Added a new SVG file for the illustration "Focus on What Matters" to enhance visual content.
    - Introduced a TypeScript configuration file (tsconfig.json) for improved editor experience and project structure.

---
+++ docusaurus.config.ts
@@ -0,0 +1,84 @@
+import {themes as prismThemes} from 'prism-react-renderer';
+import type {Config} from '@docusaurus/types';
+import type * as Preset from '@docusaurus/preset-classic';
+
+const config: Config = {
+  title: 'Fridzsi',
+  tagline: 'Fridzsi are cool',
+  favicon: 'img/favicon.ico',
+  future: {
+    v4: true,
+  },
+  url: 'https:/bencso.hu',
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/',
+  organizationName: 'Bencso',
+  projectName: 'fridzsi',
+  onBrokenLinks: 'throw',
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+  presets: [
+    [
+      'classic',
+      {
+        docs: {
+          sidebarPath: './sidebars.ts',
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        blog: false,
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    image: 'img/docusaurus-social-card.jpg',
+    colorMode: {
+      respectPrefersColorScheme: true,
+    },
+    navbar: {
+      title: 'Fridzsi',
+      items: [
+        {
+          type: 'docSidebar',
+          sidebarId: 'mainSidebar',
+          position: 'left',
+          label: 'Documentation',
+        },
+        {
+          href: 'https://github.com/bencso/fridzsi',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Socials',
+          items: [
+            {
+              label: 'GitHub',
+              href: 'https://github.com/bencso/fridzsi',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} Bencso. Built with Docusaurus.`,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  } satisfies Preset.ThemeConfig,
+};
+
+export default config;
