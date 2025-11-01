e3580e9b4f7e82588030052d84ce832fcc718931
bencso <bencsoofficial@gmail.com>
2025-11-01 11:56:32 +0100

    feat: Update dependencies and add Tailwind CSS support
    - Added new dependencies: @astrojs/starlight-tailwind, @fontsource-variable/dm-sans, @pelagornis/page, and @tailwindcss/vite.
    - Included Tailwind CSS in global styles.
    - Removed outdated system requirements documentation in both English and Hungarian.
    - Created new splash pages for English and Hungarian documentation with updated content.
    - Implemented new layout components for better page structure and navigation.
    - Added system requirements documentation for English and Hungarian versions.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,54 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+import tailwindcss from "@tailwindcss/vite";
 
-// https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'Fridzsi - dokumentáció',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/bencso/fridzsi' }],
-			sidebar: [
-				{
-					label: "Első lépések",
-					autogenerate: { directory: 'docs/getting-started' },
-				},
-			],
-		}),
-	],
+  integrations: [
+    starlight({
+      components: {
+        PageFrame: "./src/components/PageFrame.astro",
+        TwoColumnContent: "./src/components/TwoColumnContent.astro",
+      },
+      customCss: ["./src/styles/global.css", "@fontsource-variable/dm-sans"],
+      title: {
+        en: "Fridzsi - User documentation",
+        hu: "Fridzsi - Felhasználói dokumentáció",
+      },
+      social: [
+        {
+          icon: "github",
+          label: "GitHub",
+          href: "https://github.com/bencso/fridzsi",
+        },
+      ],
+      sidebar: [
+        {
+          label: "Első lépések",
+          translations: {
+            en: "Getting started",
+          },
+          autogenerate: {
+            directory: "/getting-started",
+          },
+        },
+      ],
+      defaultLocale: "hu",
+      locales: {
+        hu: {
+          label: "Magyar",
+          lang: "hu",
+        },
+        en: {
+          label: "English",
+          lang: "en",
+        },
+      },
+      disable404Route: true,
+    }),
+  ],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
